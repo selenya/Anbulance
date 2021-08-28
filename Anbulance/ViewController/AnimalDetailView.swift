ece9be4b3a0791043d245e1e812e27213397d4a3
Selen <selenyanar@gmail.com>
2021-08-28 11:52:48 +0300

    Initial Commit

---
+++ Anbulance/ViewController/AnimalDetailView.swift
@@ -0,0 +1,54 @@
+//
+//  AnimalDetailView.swift
+//  Anbulance
+//
+//  Created by Selen Yanar on 10/08/21.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseUI
+import SDWebImageSwiftUI
+
+struct AnimalDetailView: View {
+    
+    var animalAnnotation: AnimalAnnotation?
+    
+    var body: some View {
+        VStack {
+            
+            // USE PLACEHOLDER IMAGE IF NONE AVAILABLE
+            let imageUrl = animalAnnotation?.imageUrl ?? "https://via.placeholder.com/468x300?text=No%20image"
+            WebImage(url: URL(string: imageUrl)).resizable().scaledToFit()
+                .frame(width: 350.0, height: 300.0)
+                .cornerRadius(8)
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 1))
+            
+            Text(animalAnnotation?.title ?? "")
+                .frame(width: 350.0, height: 250.0)
+            Button(
+                action: {
+                    print("Delete annotation")
+                },
+                label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .frame(width: 350, height: 50, alignment: .center)
+                            .foregroundColor(Color("AnbulanceBlue"))
+                            .cornerRadius(8)
+                        Text("Kurtarıldı")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                    }
+                })
+        }
+        
+    }
+    
+    struct ReportView_Previews: PreviewProvider {
+        static var previews: some View {
+            AnimalDetailView()
+        }
+    }
+}
