8f1a5d89e84a636428ea4f1bd624f582b1714b1c
Selen <selenyanar@gmail.com>
2021-10-02 12:48:05 +0300

    Report button added to AnimalDetailView

--- Anbulance/ViewController/AnimalDetailView.swift
+++ Anbulance/ViewController/AnimalDetailView.swift
@@ -9,24 +9,25 @@ import Foundation
 import SwiftUI
 import FirebaseUI
 import SDWebImageSwiftUI
+import FirebaseFirestore
 
 struct AnimalDetailView: View {
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State private var showAlert = false
-    
-    
+    @State var showFlagView = false
     
     var btnBack : some View {
         Button(
             action: {
-        self.presentationMode.wrappedValue.dismiss()
-    }) {
-        Text("< Geri")
-            .foregroundColor(Color("AnbulanceBlue"))
-        
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+            Text("< Geri")
+                .foregroundColor(Color("AnbulanceBlue"))
+            
+        }
     }
-    }
+    
     
     var animalAnnotation: AnimalAnnotation?
     
@@ -78,25 +79,67 @@ struct AnimalDetailView: View {
                 }).alert(isPresented: $showAlert,
                          content: {
                             Alert(
-                                        title: Text("Yaralı hayvan kurtarıldı mı?"),
-                                        message: Text("Evet'i seçersen bu gönderi haritadan silinecek. Bu işlem geri alınamaz."),
-                                        primaryButton: .cancel(Text("İptal")),
-                                        secondaryButton: .destructive(
-                                            Text("Evet"),
-                                            action: {FirebaseService().deletePost(withID: animalAnnotation!.postID!); presentationMode.wrappedValue.dismiss()
-                                            }
-                                        ))
-                })
+                                title: Text("Yaralı hayvan kurtarıldı mı?"),
+                                message: Text("Evet'i seçersen bu gönderi haritadan silinecek. Bu işlem geri alınamaz."),
+                                primaryButton: .cancel(Text("İptal")),
+                                secondaryButton: .destructive(
+                                    Text("Evet"),
+                                    action: {FirebaseService().deletePost(withID: animalAnnotation!.postID!); presentationMode.wrappedValue.dismiss()
+                                    }
+                                ))
+                         })
+            Button(action: {
+                self.showFlagView.toggle()
+            }, label: {
+                Text("Bu gönderiyi bildir")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("AnbulanceBlue"))
+                    .padding(.leading, 180.0)
+            })
+            .padding(.top, 10)
+            .padding(.bottom, 50.0)
+            .alert(isPresented: $showFlagView,
+                     content: {
+                        Alert(
+                            title: Text("Bu gönderiyi bildirmek istiyor musun?"),
+                            message: Text("Eğer bu gönderinin uygulamanın kullanım amacına aykırı olduğunu düşünüyorsan 'Evet'i seç."),
+                            primaryButton: .cancel(Text("İptal")),
+                            secondaryButton: .destructive(
+                                Text("Evet, bu gönderiyi bildir."),
+                                action: {
+                                    
+                                    addReportData()
+                                    presentationMode.wrappedValue.dismiss()
+                                }
+                            ))
+                     })
         }
+        
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: btnBack)
     }
-
     
+    
+    func addReportData() {
+        let db = Firestore.firestore()
+        db.collection("reported").document(animalAnnotation!.postID!).setData([
+            "postID": animalAnnotation!.postID!
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added successfully.")
+            }
+        }
+        
+    }
+
     
     struct ReportView_Previews: PreviewProvider {
         static var previews: some View {
             AnimalDetailView()
         }
     }
+ 
+    
 }
